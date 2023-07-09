6d48e9bd688e1dffefb242e07620ccff7c88c37c
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2023-07-08 22:42:51 -0700

    added a function to identify if clicked number is a operator or a number

--- calc.js
+++ calc.js
@@ -6,16 +6,43 @@ const resultDiv = document.querySelector(".result");
 let numberEntered = "";
 let operator = "";
 let isOperator = false;
-let operatorArray = ["+", "-", "/", "*"];
+let operatorArray = ["+", "-", "/", "*", "%", "="];
 numberButtons.forEach((item) => {
   item.addEventListener("click", () => {
+    identifyInput(item.innerText, operatorArray);
     displayNumber(item.innerText);
   });
 });
 
 displayNumber = (number) => {
   numberEntered = numberEntered + number;
-  console.log(numberEntered);
 
   resultDiv.innerText = numberEntered;
+};
+
+identifyInput = (input, array) => {
+  if (array.includes(input)) performOperation(input);
+};
+
+performOperation = (operator) => {
+  switch (operator) {
+    case "+":
+      console.log("add");
+      break;
+    case "-":
+      console.log("subtract");
+      break;
+    case "/":
+      console.log("divide");
+      break;
+    case "*":
+      console.log("multiply");
+      break;
+    case "%":
+      console.log("modulo");
+      break;
+    case "=":
+      console.log("perform operations");
+      break;
+  }
 };
