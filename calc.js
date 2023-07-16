fa9ed1b314363d85906be18c29c3c44ee138111f
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2023-07-15 22:07:21 -0700

    added the backspace functionalit

--- calc.js
+++ calc.js
@@ -1,20 +1,23 @@
 /** @format */
+
 const numberButtons = document.querySelectorAll(".button");
 const resultDiv = document.querySelector(".result");
 const expressionDiv = document.querySelector(".expression");
-
-//let enteredNumbers = [];
-
+const deleteButton = document.getElementById("DEL");
+// variables initial assignment
 let numberEntered = "";
 let operator = "";
 let queueArray = [];
 let isOperator = false;
 let operatorArray = ["+", "-", "/", "*", "%", "="];
 let disNumber = "";
+
+//when a button is clicked we send the button's value to display area and also to identify if it an operator or an operand
 numberButtons.forEach((item) => {
   item.addEventListener("click", () => {
     disNumber = displayNumber(item.innerText);
-    //console.log(item.innerText);
+
+    //our identify input function
     identifyInput(disNumber, operatorArray, item.innerText);
 
     if (item.innerText == "AC") {
@@ -22,12 +25,25 @@ numberButtons.forEach((item) => {
     }
   });
 });
-
+// our number gets displayed in the display area using this function
 displayNumber = (number) => {
+  //clears the screen when AC is pressed
   if (number == "AC") {
     clearScreen();
     return 0;
-  } else {
+  }
+  //backspace operation peformed here
+  else if (number == "DEL") {
+    numberEntered = numberEntered.substring(0, numberEntered.length - 1);
+    if (numberEntered != "") {
+      resultDiv.innerText = numberEntered;
+      return numberEntered;
+    } else {
+      resultDiv.innerText = 0;
+    }
+  }
+  //diplay the number
+  else {
     numberEntered = numberEntered + number;
 
     resultDiv.innerText = numberEntered;
@@ -35,39 +51,40 @@ displayNumber = (number) => {
   }
 };
 
+// to clear the screen when 'AC' button is clicked
 clearScreen = () => {
   resultDiv.innerText = "0";
   expressionDiv.innerText = "";
-  console.log(numberEntered);
+
   numberEntered = "";
   queueArray = [];
 };
 
+//identify's the input
 identifyInput = (dNum, opArray, cItem) => {
   if (opArray.includes(cItem)) {
     pushNum = dNum.substring(0, dNum.length - 1);
 
     queueArray.push(pushNum);
-    console.log(queueArray);
 
-    //resultDiv.innerText = "";
     if (cItem == "=") {
       performOperation(queueArray, dNum);
     }
   }
-};
 
-performOperation = (qArray, dNum) => {
-  let result = eval(`${qArray[qArray.length - 1]}`);
-  //let expression = qArray[qArray.length - 1];
-  resultDiv.innerText = result;
+  //performs required operation
+  performOperation = (qArray, dNum) => {
+    let result = eval(`${qArray[qArray.length - 1]}`);
 
-  addExpression(dNum);
-};
+    resultDiv.innerText = result;
 
-addExpression = (dNum) => {
-  const newtext = document.createTextNode(dNum);
-  const p1 = document.getElementById("expressiondiv");
+    addExpression(dNum);
+  };
+  //adds the expression on the top of the result
+  addExpression = (dNum) => {
+    const newtext = document.createTextNode(dNum);
+    const p1 = document.getElementById("expressiondiv");
 
-  p1.appendChild(newtext);
+    p1.appendChild(newtext);
+  };
 };
